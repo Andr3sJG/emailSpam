497409ea27e456594b6d98d87c7ddd288a47782b
Andr3sJG <andresjoseis@gmail.com>
2021-06-16 16:31:17 -0700

    main file

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
-import(
+
+import (
 	"fmt"
+	"net/http"
 )
 
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Hello there <h1>")
+}
 
-func main(){
+func main() {
 	fmt.Println("hi onichan")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
