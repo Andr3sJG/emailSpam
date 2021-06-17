fe33077ef07f7dc2cb3265dce6aade3ef951b0f7
Andr3sJG <andresjoseis@gmail.com>
2021-06-17 15:04:25 -0700

    new main

--- main.go
+++ main.go
@@ -9,25 +9,14 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1> Hello there <h1>")
-}
 
-func determineAddress() (string, error) {
-	port := os.Getenv("PORT")
-	if port == "" {
-		return "", fmt.Errorf("$PORT not set")
-	}
-	return ":" + port, nil
 }
 
 func main() {
 
-	_, err := determineAddress()
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", handlerFunc)
+	log.Print(port)
+	http.ListenAndServe(":"+port, nil) //local machine
 
-	//http.ListenAndServe(":3000", nil)//local machine
-	//http.ListenAndServe(port, nil)
 }
