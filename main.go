a90cbee9330f9855d3503a0dd5b7cd6edfab6336
Andr3sJG <andresjoseis@gmail.com>
2021-06-16 20:49:42 -0700

    changed main

--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	//"os"
+	"os"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1> Hello there <h1>")
 }
 
+func determineAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
 func main() {
 
-	//port := os.Getenv("PORT")
-	fmt.Println("hi onichan")
+	_, err := determineAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", handlerFunc)
+
 	//http.ListenAndServe(":3000", nil)//local machine
 	//http.ListenAndServe(port, nil)
 }
